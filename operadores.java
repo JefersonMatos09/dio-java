def4e0d7838093523829ffa4c6f26cb0d907bcd6
JefersonMatos09 <decoo.matos@live.com>
2023-09-10 19:17:28 -0300

    meus dados

---
+++ operadores.java
@@ -0,0 +1,13 @@
+class static meus dados {
+    public static void main(String[] args) {
+      String = "Jeferson Matos";
+      int numero = 29;
+      String cidade = "Américo Brasiliense-SP";
+      String profissao = "Motorista Canavieiro";
+      
+      System.out.println();
+     
+     
+        System.out.println();   
+    }
+}
