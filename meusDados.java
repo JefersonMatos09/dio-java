e75bd738a44d3684c21199eddbfa4a0aaa47dd2c
JefersonMatos09 <decoo.matos@live.com>
2023-09-10 19:54:45 -0300

    meus-dados

---
+++ meusDados.java
@@ -0,0 +1,18 @@
+public class meusdados {
+
+
+    public static void main(String[] args) {
+      String nomeString = "Jeferson Matos";
+      int idade = 29;
+      String cidade = "Américo Brasiliense-SP";
+      String profissao = "Motorista Canavieiro";
+
+      profissao = "Programador Back-end";
+    
+        System.out.println("Qual o seu nome ? "+ nomeString); 
+        System.out.println("Qual a sua idade ? "+ idade);   
+        System.out.println("Qual a cidade você mora ? "+ cidade);
+        System.out.println("Qual a sua profissão ? " + profissao);
+    }
+}
+
