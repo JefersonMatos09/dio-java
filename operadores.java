34ca3fc132e77c3566bf64aae9829757ffeafacf
JefersonMatos09 <decoo.matos@live.com>
2023-09-10 20:40:19 -0300

    operadores.java

--- operadores.java
+++ operadores.java
@@ -1,6 +1,26 @@
 public class operadores {
+ // Exemplo Usando IF ELSE ;
     public static void main(String[] args) {
+       
         int a, b; 
+
+        a = 6;
+        b = 10;
+String resultado = "";
+if(a==b)
+resultado = "Verdadeiro";
+else
+resultado = "False";
+  
+
+     System.out.println(resultado);
+        
+    }
+}
+
+// Exemplo usando ? no lugar do IF , e : no lugar de ELSE;
+ 
+          int a, b; 
 
         a = 6;
         b = 10;
@@ -9,6 +29,4 @@ public class operadores {
   
 
      System.out.println(resultado);
-        
-    }
-}
+ 
