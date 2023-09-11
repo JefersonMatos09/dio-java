cad80266406998a7600bc943ffde1e866b9b5bc3
JefersonMatos09 <decoo.matos@live.com>
2023-09-11 16:19:33 -0300

    operadores-logicos

---
+++ operadoresLogicos.java
@@ -0,0 +1,14 @@
+public class operadoresLogicos {
+    public static void main(String[] args) {
+        boolean estrutura1 = true;
+        boolean  estrutura2 = false;
+
+        if(estrutura1 && estrutura2){
+            System.out.println("as duas estruturas são verdadeiras");
+        }
+        if(estrutura1 || estrutura2);
+        System.out.println("Uma das estruturas é verdadeira");
+System.out.println("FIM");
+    }
+}
+
